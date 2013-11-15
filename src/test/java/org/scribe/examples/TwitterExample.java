bb9d0e2f03be6cdfe575eb8e4950bda32e09bbdb
Christopher A. Baumbauer <cab@cabnetworks.net>
2013-11-15 11:29:36 -0800

    Corrected the Twitter request to account for the example no longer making a post.

--- src/test/java/org/scribe/examples/TwitterExample.java
+++ src/test/java/org/scribe/examples/TwitterExample.java
@@ -46,8 +46,7 @@ public class TwitterExample
 
     // Now let's go and ask for a protected resource!
     System.out.println("Now we're going to access a protected resource...");
-    OAuthRequest request = new OAuthRequest(Verb.POST, PROTECTED_RESOURCE_URL);
-    request.addBodyParameter("status", "this is sparta! *");
+    OAuthRequest request = new OAuthRequest(Verb.GET, PROTECTED_RESOURCE_URL);
     service.signRequest(accessToken, request);
     Response response = request.send();
     System.out.println("Got it! Lets see what we found...");
